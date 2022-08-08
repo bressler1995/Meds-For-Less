8320e895b35a80394f0fa1ba09edfdccc89b0ab3
carlofabyss <carlofabyss@hotmail.com>
2022-08-08 12:10:57 -0700

    Override HTML Template
    Still needs content

--- woocommerce/emails/admin-new-order.php
+++ woocommerce/emails/admin-new-order.php
@@ -16,53 +16,17 @@
  */
 
 defined( 'ABSPATH' ) || exit;
-$the_orderID = $order->get_id();
-$order_data = $order->get_data();
-$order_uid = $order->get_user_id();
+
+?>
 
 
-
-for($z = 0; $z < count($order_data); $z++) {
-    echo var_dump($order_data[$z]) . '<br><br>';
-}
-
-/*
- * @hooked WC_Emails::email_header() Output the email header
- */
-do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
-
-<?php /* translators: %s: Customer billing full name */ ?>
-<p><?php printf( esc_html__( 'Patient Name: %s', 'woocommerce' ), $order->get_formatted_billing_full_name() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p><p>Patient Date Of Birth: <?php echo do_shortcode('[medsforless_showfield field="birthday"]'); ?></p><p>Patient Phone Number: <?php echo do_shortcode('[medsforless_showfield field="phone"]'); ?></p>
-<?php
-
-/*
- * @hooked WC_Emails::order_details() Shows the order details table.
- * @hooked WC_Structured_Data::generate_order_data() Generates structured data.
- * @hooked WC_Structured_Data::output_structured_data() Outputs structured data.
- * @since 2.5.0
-*/
-
-do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email );
-
-/*
- * @hooked WC_Emails::order_meta() Shows order meta data.
- */
-do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
-
-/*
- * @hooked WC_Emails::customer_details() Shows customer details
- * @hooked WC_Emails::email_address() Shows email address
- */
-do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
-
-/**
- * Show user-defined additional content - this is set in each email's settings.
- */
-if ( $additional_content ) {
-	echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
-}
-
-/*
- * @hooked WC_Emails::email_footer() Output the email footer
- */
-do_action( 'woocommerce_email_footer', $email );
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>" />
+		<title><?php echo get_bloginfo( 'name', 'display' ); ?></title>
+	</head>
+	<body>
+		Hello World HTML Email Override
+	</body>
+</html>
